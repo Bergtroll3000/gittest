638b7cd34b438bf947e724a512058147569ab572
ReuteM1 <ReuteM1@IR1-PC-08-W7.schule.local>
2015-10-21 16:49:56 +0200

    change connis brain

---
+++ git1.0/src/git1/pkg0/Git10.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git1.pkg0;
+
+/**
+ *
+ * @author ReuteM1
+ */
+public class Git10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
